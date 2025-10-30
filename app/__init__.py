05666fc4b1a37b6cd20ac3c4e165b9a7758ebaab
bapdodi <arlawjdqls12@gmail.com>
2025-10-30 17:01:11 +0900

    Revert "Update __init__.py"
    This reverts commit 4280e0c7a44f9b2fddb5e38d14aa562ac4e64c40.

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://www.planmate.site"],
+        allow_origins=settings.allowed_origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
